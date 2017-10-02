4e512fd30bf4c841c0ac8dd6672a45eaf5ab4f13
Tõnis Ojandu <tonis.ojandu@gmail.com>
2017-10-02 23:48:54 +0300

    memory twiekes

--- scripts/7-build-vocabulary.sh
+++ scripts/7-build-vocabulary.sh
@@ -12,7 +12,7 @@
 #The maximum walltime of the job is a 1 day
 #SBATCH -t 24:00:00
 
-#SBATCH --mem=150G
+#SBATCH --mem=80G
 
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:tesla:1
@@ -24,7 +24,7 @@ source env.sh
 echo
 echo Starting to create dictionary
 
-python $ROOT/OpenNMT-py/preprocess.py -train_src $DDIR/bpe.cleaned-tc-tok-train.et -train_tgt $DDIR/bpe.cleaned-tc-tok-train.en -valid_src $DDIR/bpe.tc-tok-dev.et -valid_tgt $DDIR/bpe.tc-tok-dev.en -save_data $DDIR/rdy -src_vocab_size 10000 -tgt_vocab_size 10000 -seed 123
+python $ROOT/OpenNMT-py/preprocess.py -train_src $DDIR/bpe.cleaned-tc-tok-train.et -train_tgt $DDIR/bpe.cleaned-tc-tok-train.en -valid_src $DDIR/bpe.tc-tok-dev.et -valid_tgt $DDIR/bpe.tc-tok-dev.en -save_data $DDIR/rdy -src_vocab_size 30000 -tgt_vocab_size 30000 -seed 123
 
 echo
 echo Dictionary created

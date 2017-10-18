f6e902d92eb8aad21a561c003fb5b912bf4e2944
Tõnis Ojandu <tonis.ojandu@gmail.com>
2017-10-18 10:19:10 +0300

    recalc BLEU

--- scripts/9-translate.sh
+++ scripts/9-translate.sh
@@ -21,6 +21,6 @@ module load python-2.7.13
 
 source env.sh
 
-stdbuf -oL python $ROOT/OpenNMT-py/translate.py -model $ROOT/models/toneko-model_acc_67.81_ppl_4.94_e8.pt -src $DDIR/bpe.tc-tok-test.et -output $ROOT/hyps/hyps.en -gpu 0
+stdbuf -oL python $ROOT/OpenNMT-py/translate.py -model $ROOT/models/toneko-model_acc_71.01_ppl_4.08_e13.pt -src $DDIR/bpe.tc-tok-test.et -output $ROOT/hyps/hyps.en -gpu 0
 
 echo Done!

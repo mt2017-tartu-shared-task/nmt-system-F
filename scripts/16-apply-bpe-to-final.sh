55229685d57a56ebb150f7a82a8ac26d48e128fa
Tõnis Ojandu <tonis.ojandu@gmail.com>
2017-12-21 17:06:33 +0200

    final translation scripts

--- scripts/16-apply-bpe-to-final.sh
+++ scripts/16-apply-bpe-to-final.sh
@@ -22,12 +22,14 @@ for lang in et ; do
 
 	echo Working with language: $lang
 
-	$ROOT/OpenNMT-py/tools/tokenizer.perl < $MROOT/lab3/accurat-dev/final.$lang > $DDIR/tok-final.$lang
+	$ROOT/OpenNMT-py/tools/tokenizer.perl < $DDIR/final.$lang > $DDIR/tok-final.$lang
 	$ROOT/OpenNMT-py/tools/truecase.perl --model $DDIR/$lang-truecase.mdl < $DDIR/tok-final.$lang > $DDIR/tc-tok-final.$lang
 
 done
 
-echo Starting to apply BPE to accurate
+$ROOT/OpenNMT-py/tools/clean-corpus-n.perl $DDIR/tc-tok-final et et $DDIR/cleaned-tc-tok-final 1 100
+
+echo Starting to apply BPE to final
 
 stdbuf -oL  python $ROOT/OpenNMT-py/tools/subword-nmt/apply_bpe.py -c $DDIR/eten.bpe < $DDIR/cleaned-tc-tok-final.et > $DDIR/bpe.final.et
 
